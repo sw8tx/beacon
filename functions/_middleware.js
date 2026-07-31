ecadec6cd47cba3f67e27a45c92c9bed3aa43033
sw8tx <tylerosthoff3@gmail.com>
2026-08-01 00:08:11 +0200

    Redirect www to apex domain

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,5 +1,9 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
+  if (url.hostname === "www.beacon-bot.site") {
+    url.hostname = "beacon-bot.site";
+    return Response.redirect(url.toString(), 301);
+  }
   if (url.hostname === "status.beacon-bot.site" && (url.pathname === "/" || url.pathname === "/index.html")) {
     url.pathname = "/status/";
     return context.next(new Request(url, context.request));
