8c2bad85b23e277e863f0b912dc329cab13a1c28
sw8tx <tylerosthoff3@gmail.com>
2026-07-31 22:56:02 +0200

    Keep status page on subdomain root

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
   if (url.hostname === "status.beacon-bot.site" && (url.pathname === "/" || url.pathname === "/index.html")) {
     url.pathname = "/status/";
-    return Response.redirect(url.toString(), 302);
+    return context.next(new Request(url, context.request));
   }
   return context.next();
 }
